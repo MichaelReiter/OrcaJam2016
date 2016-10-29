ab51f97d2ae0ad30e04d4858d683af7a7a322c42
Michael Reiter <michaelreiter@live.ca>
2016-10-29 09:36:30 -0700

    Fix js build

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
-// import {MenuState} from 'MenuState';
-
 const GameResolution = {
   width: 640,
   height: 960  
 };
 
 let game = null;
-let music = null;
 
-function main() {
+const main = () => {
   game = new Phaser.Game(GameResolution.width, GameResolution.height, Phaser.AUTO, 'game');
-  // game.state.add('Menu', MenuState, true);
-  // game.state.add('Gameplay', GameplayState, false);
+  game.state.add('Menu', MenuState, true);
+  game.state.add('Gameplay', GameplayState, false);
 };
