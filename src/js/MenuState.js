27d0237debfc8eeb05d501f1925f7e5e386749fa
Michael Reiter <michaelreiter@live.ca>
2016-10-28 21:07:30 -0700

    Update boilerplate

--- src/js/MenuState.js
+++ src/js/MenuState.js
@@ -1,4 +1,4 @@
-const MenuState = {
+export const MenuState = {
   preload: () => {
 
   },
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-// Scripts here
+// import {MenuState} from 'MenuState';
+
+const GameResolution = {
+  width: 640,
+  height: 960  
+};
+
+let game = null;
+let music = null;
+
+function main() {
+  game = new Phaser.Game(GameResolution.width, GameResolution.height, Phaser.AUTO, 'game');
+  // game.state.add('Menu', MenuState, true);
+  // game.state.add('Gameplay', GameplayState, false);
+};
