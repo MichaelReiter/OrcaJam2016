0740f91ac9bd16e9d6dc49bdd3d16a63ebaee278
Michael Reiter <michaelreiter@live.ca>
2016-10-28 21:06:01 -0700

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,81 @@
 const gulp       = require('gulp');
+const gutil      = require('gulp-util');
+const concat     = require('gulp-concat');
+// const sass       = require('gulp-sass');
+const changed    = require('gulp-changed');
+const imagemin   = require('gulp-imagemin');
+const minifyHTML = require('gulp-minify-html');
 const babel      = require('gulp-babel');
-const stylus     = require('gulp-stylus');
-const livereload = require('gulp-livereload');
 
-const htmlSource   = './src/html/*.html';
-const stylusSource = './src/styles/*.styl';
-const jsSource     = './src/js/*.js';
-const assetsSource = './assets/*';
-const destination  = './build';
+const jsSrc = [
+  './src/js/main.js',
+  './src/js/*.js'
+];
+const jsDst    = './build/js';
+const libSrc   = './src/lib/*.js';
+const libDst   = './build/lib';
+const imgSrc   = './src/img/*';
+const imgDst   = './build/img';
+const htmlSrc  = './src/html/*.html';
+const htmlDst  = './build/';
+// const audioSrc = './src/audio/*';
+// const audioDst = './build/audio';
+// const styleSrc = './src/styles/*.scss';
+// const styleDst = './build/css';
 
-// Build ES6 scripts
-gulp.task('js', () => {
-  return gulp.src(jsSource)
+// Build JavaScript files
+gulp.task('javascript', () => {
+  gulp.src(jsSrc)
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest(destination))
-    .pipe(livereload());
+    .pipe(gulp.dest(jsDst))
 });
 
-// Build Stylus stylesheets
-gulp.task('styl', () => {
-  return gulp.src(stylusSource)
-    .pipe(stylus())
-    .pipe(gulp.dest(destination))
-    .pipe(livereload());
+// Copy JavaScript libraries
+gulp.task('lib', () => {
+  gulp.src(libSrc).pipe(gulp.dest(libDst));
 });
 
-// Copy markup
+// Copy image files
+gulp.task('img', () => {
+  gulp.src(imgSrc).pipe(gulp.dest(imgDst));
+});
+
+// Copy html files
 gulp.task('html', () => {
-  return gulp.src(htmlSource)
-    .pipe(gulp.dest(destination))
-    .pipe(livereload());
+  gulp.src(htmlSrc).pipe(minifyHTML()).pipe(gulp.dest(htmlDst));
 });
 
-// Copy assets
-gulp.task('assets', () => {
-  return gulp.src(assetsSource)
-    .pipe(gulp.dest(destination))
-    .pipe(livereload());
-});
+// // Build stylesheet files
+// gulp.task('style', () => {
+//   gulp.src(styleSrc).pipe(sass()).pipe gulp.dest(styleDst)
+// });
 
-// Build everything and watch for changes
+// // Copy audio
+// gulp.task('audio', () => {
+//   gulp.src(audioSrc).pipe gulp.dest(audioDst);
+// });
+
+// Default task((this is run when you type 'gulp' in the root directory)
 gulp.task('default', [
-    'html',
-    'styl',
-    'js',
-    'assets'
-  ], () => {
-    livereload.listen();
-    gulp.watch(htmlSource, ['html']);
-    gulp.watch(jsSource, ['js']);
-    gulp.watch(stylusSource, ['styl']);
-    gulp.watch(assetsSource, ['assets']);
-  }
-);
+  'javascript',
+  'lib',
+  'html',
+  'img',
+  // 'audio',
+  // 'style',
+  // 'fonts'
+], () => {
+  // watch for changes in JavaScript files
+  gulp.watch(jsSrc, ['javascript']);
+  // watch for changes in image files
+  gulp.watch(imgSrc, ['img']);
+  // watch for changes in html files
+  gulp.watch(htmlSrc, ['html']);
+  // watch for changes in stylesheets
+  // gulp.watch(styleSrc, ['style']);
+  // watch for changes in fonts
+  // watch for changes in audio
+  // gulp.watch(styleSrc, ['audio']);
+});
